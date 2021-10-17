a14cf40b532e0cbb632203cab7bd0989e44670b7
kamruz-zaman8991 <70217992+kamruz-zaman8991@users.noreply.github.com>
2021-10-17 15:23:26 +0600

    final customize login anf signup form

--- src/component/Header/LogIn/Login.js
+++ src/component/Header/LogIn/Login.js
@@ -4,15 +4,15 @@ import logo from '../../../Img/logo/logo2.png'
 
 const Login = () => {
     return (
-        <div className='flex justify-center items-center'>
-            <div>
-                <img className='w-64' src={logo} alt="" />
+        <div className='flex justify-center items-center' >
+            <div className=' p-10 rounded-xl shadow-xl'>
+                <img className='w-64 mb-6' src={logo} alt="" />
                 <input className='bg-transparent border border-red-500 p-2 m-1 rounded-lg w-full' type="email" name="email" placeholder='Emal' id="" /><br />
                 <input className='bg-transparent border border-red-500 p-2 m-1 rounded-lg w-full' type="password" name="pass" placeholder='Password' id="" /><br />
-                <button className='bg-red-500 w-full m-2 p-2 rounded-lg hover:bg-red-800'>SignIn</button>
-                <p>Didn't have account? <Link to='/regestration' className='text-blue-600'>SignUp</Link></p>
+                <button className='bg-red-500 w-full m-2 p-2 rounded-lg hover:bg-red-800'>LogIn</button>
+                <p className='text-center'>Didn't have account? <Link to='/regestration' className='text-blue-600'>SignUp</Link></p>
                 <div className='text-center mt-5'>
-                    <p className='text-pink-900'>OR, SignIn with</p>
+                    <p className='text-pink-900'>OR, LogIn with</p>
                     <button className='m-5'><i className="fab fa-facebook text-4xl"></i></button>
                     <button className='m-5'><i className="fab fa-google text-4xl"></i></button>
                     <button className='m-5'><i className="fab fa-github text-4xl"></i></button>
--- src/component/Header/Regestration/Regedtration.js
+++ src/component/Header/Regestration/Regedtration.js
@@ -5,14 +5,14 @@ import logo from '../../../Img/logo/logo2.png'
 const Regedtration = () => {
     return (
         <div className='flex justify-center items-center'>
-            <div>
-                <img className='w-64' src={logo} alt="" />
+            <div className=' p-10 rounded-xl shadow-xl'>
+                <img className='w-64 mb-6' src={logo} alt="" />
                 <input className='bg-transparent border border-red-500 p-2 m-1 rounded-lg w-full' type="text" name="name" id="" placeholder='Name' /><br />
                 <input className='bg-transparent border border-red-500 p-2 m-1 rounded-lg w-full' type="email" name="email" placeholder='Emal' id="" /><br />
                 <input className='bg-transparent border border-red-500 p-2 m-1 rounded-lg w-full' type="password" name="pass" placeholder='Password' id="" /><br />
                 <input className='bg-transparent border border-red-500 p-2 m-1 rounded-lg w-full' type="password" name="confirm pass" placeholder='Confirm Password' id="" />
                 <button className='bg-red-500 w-full m-2 p-2 rounded-lg hover:bg-red-800'>SignUp</button>
-                <p>Already have an account? <Link to='/signin' className='text-blue-600'>SignIn</Link></p>
+                <p>Already have an account? <Link to='/login' className='text-blue-600'>SignIn</Link></p>
                 <div className='text-center mt-5'>
                     <p className='text-pink-900'>OR, SignUp with</p>
                     <button className='m-5'><i className="fab fa-facebook text-4xl"></i></button>
